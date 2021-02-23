78cc6528c1d671e020e3b876b36e081639026acd
ifui <ifui@foxmail.com>
2021-02-23 19:39:28 +0800

    安装并配置 dingo/api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+$api = app('Dingo\Api\Routing\Router');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+$params_v1 = [
+    'version' => 'v1',
+    'namespace' => 'App\Http\Controllers',
+];
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api->group($params_v1, function ($api) {
 });
