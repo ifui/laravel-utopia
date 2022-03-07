00d75748d4cb53b64d18ca11945384b60d933c97
ifui <ifui@foxmail.com>
2022-03-07 19:34:42 +0800

    package: add laravel-permission

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 当角色为 super-admin 时，can() 方法返回 true
+        // 当权限为 administrator 时，can() 方法返回 true
+        Gate::before(function ($user, $ability) {
+            if ($user->hasRole('super-admin')) {
+                return true;
+            }
+            if ($user->hasAnyPermission(['administrator'])) {
+                return true;
+            }
+            return null;
+        });
     }
 }
