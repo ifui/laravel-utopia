db8d26bd48a8260bbac9c9507b755631b850cd4e
ifui <ifui@foxmail.com>
2022-04-03 18:17:52 +0800

    fix: 更改路由中间件为 web

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,7 +30,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::prefix('api')
-                ->middleware('api')
+                ->middleware('web')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
